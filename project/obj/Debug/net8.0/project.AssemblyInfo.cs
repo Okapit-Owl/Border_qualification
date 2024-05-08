a329863b9f5029496bfaf21ae08f7564ebf6ee10
Oksana <okapit@yandex.ru>
2024-05-08 19:16:23 +0400

    another commit

--- project/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ project/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d352c224048280771ce529c3fd5dc1055c5cbfde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdeea299c574020ccab492be1ddadba446469f91")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
